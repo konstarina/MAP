01350ad2a21780a1eadac18cc6ed2599f37fa549
konstarina <55761341+konstarina@users.noreply.github.com>
2021-03-11 23:29:16 +0200

    Add files via upload

--- src/main/java/com/example/pam/MainActivity.kt
+++ src/main/java/com/example/pam/MainActivity.kt
@@ -6,12 +6,34 @@ import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.pam.ui.Todo
+import com.example.pam.ui.TodoAdapter
+import kotlinx.android.synthetic.main.fragment_home.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var todoAdapter: TodoAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(findViewById(R.id.toolbar))
+        todoAdapter = TodoAdapter(mutableListOf())
+
+        rvTodoItems.adapter = todoAdapter
+        rvTodoItems.layoutManager = LinearLayoutManager(this)
+
+        btnAddToDo.setOnClickListener{
+            val todoTitle = etTodoTitle.text.toString()
+            if(todoTitle.isNotEmpty()){
+                val todo = Todo(todoTitle)
+                todoAdapter.addTodo(todo)
+                etTodoTitle.text.clear()
+            }
+        }
+        btnDeleteDoneToDos.setOnClickListener {
+            todoAdapter.deleteDoneTodos()
+        }
     }
 }
